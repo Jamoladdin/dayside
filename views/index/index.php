c71017035c15bd895f9f589530028a775a862ea2
Jamoladdin <33843475+Jamoladdin@users.noreply.github.com>
2019-03-26 13:17:28 +0500

    Update index.php

--- views/index/index.php
+++ views/index/index.php
@@ -1,13 +1,6 @@
-<?php 
-
-echo '<hr>';
-
-?>
-
 <div class="jumbotron">
   <div class="container text-center">
     <h1>Tasks</h1>      
-    <p><a href="http://mvcpure/index/list">List of tasks</a></p>
+    <p><a href="/index/list">List of tasks</a></p>
   </div>
 </div>
-	
