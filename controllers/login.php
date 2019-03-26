9b6868251896152956e201d39d86c883f7bc3c96
Jamoladdin <33843475+Jamoladdin@users.noreply.github.com>
2019-03-26 13:12:18 +0500

    Update login.php

--- controllers/login.php
+++ controllers/login.php
@@ -9,17 +9,15 @@ class Login  extends Controller
 	function __construct()
 	{
 		parent:: __construct();
-		// echo 'We are in Login controller';
 	}
 
 	public function index() {
-		$this->view->render('login/index');
+		$this->view->render('/login/index');
 	}
 	
 	public function run() {
 		$this->model->run();
-		$this->view->render('dashboard/index');
+		header('location: ../dashboard');
 	}
 }
-
- ?>
+?>
