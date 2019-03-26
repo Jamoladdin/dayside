9771cc3b8749164fe2348baa376d8fdb491d0e78
Jamoladdin <33843475+Jamoladdin@users.noreply.github.com>
2019-03-26 13:16:18 +0500

    Update index.php

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,21 +1,36 @@
+<div class="row">
+	<form action="/dashboard/index" method="post" class="col-md-12">
+		<div class="form-group">
+			<select name="select_sort2" id="select_sort2" class="form-control">
+				<option value="name" <?php if(Session::get('select_sort2') == 'name') echo 'selected'; ?> >Name</option>
+				<option value="email" <?php if(Session::get('select_sort2') == 'email') echo 'selected'; ?> >Email</option>
+				<option value="status"<?php if(Session::get('select_sort2') == 'status') echo 'selected'; ?> >Status</option>
+			</select>
+		</div>
+		<div class="form-group">
+			<input type="submit" value="Sort" class="btn -btn-default col-md-1">
+		</div>
+	</form>
+</div>
+<hr>
+
 <table class="table table-hover">
 	<thead>
 		<tr>
-			<th>Id</th>
-			<th>Title</th>
+			<th>Name</th>
+			<th>Email</th>
 			<th>Text</th>
 			<th>Status</th>
 			<th>Actions</th>
 		</tr>
 	</thead>
 	<tbody>
-<?php 
-// echo 'Data: '; print_r($data);
-foreach ($data as $item) {
+<?php
+foreach ($data['task'] as $item) {
 ?>
 <tr>
-	<td><?= $item['id']; ?></td>
-	<td><?= $item['title']; ?></td>
+	<td><?= $item['name']; ?></td>
+	<td><?= $item['email']; ?></td>
 	<td><?= $item['text']; ?></td>
 	<td><?php
 		if($item['status']==0) echo 'inactive';
@@ -23,11 +38,21 @@ foreach ($data as $item) {
 		?>
 	</td>
 	<td>
-		<a href="http://mvcpure/dashboard/form/<?= $item['id'] ?>">Update</a>
+		<a href="/dashboard/form/<?= $item['id'] ?>">Update</a>
 	</td>
 </tr>
 <?php
 }
-?>
-	</tbody>
+?></tbody>
 </table>
+
+<div><?php
+	if($data['paginationSize'] > 1) {
+		for ($i=1; $i<=$data['paginationSize']; $i++) {
+			?>
+			<a href="/dashboard/index/<?= $i?>" class="btn btn-default <?php if($data['page']==$i) echo 'btn-success' ?>"><?= $i?></a>
+			<?php
+		}
+	}
+	?></div>
+<hr>
