0af7021c643501dfcf58eccde3d62e1c76ef24b8
Jamoladdin <33843475+Jamoladdin@users.noreply.github.com>
2019-03-26 13:19:17 +0500

    Update header.php

--- views/header.php
+++ views/header.php
@@ -3,28 +3,25 @@
 <head>
 	<title>Test</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 </head>
 <body>
+    <?php Session::init(); ?>
 	<div class="container container-fluid">
 	<div id="header" class="navbar" style="margin-top: 10px;">
 		<div class="navbar-header">
 			<ul class="list-group">
 				<li class="list-group-item" style="display: inline-block;">
-					<a href="http://mvcpure/index">Index</a>
+					<a href="/index">Index</a>
 				</li>
 				<li class="list-group-item" style="display: inline-block;">
 					<?php if (Session::get('loggedIn') == false) { ?>
-						<a href="http://mvcpure/login">Login</a>
+						<a href="/login">Login</a>
 					<?php } else { ?>
-						<a href="http://mvcpure/dashboard/logout">Logout</a>
+						<a href="/dashboard/logout">Logout</a>
 					<?php } ?>
 				</li>
 			</ul>
 		</div>
 	</div>
 	<div id="content">
-
-		<!-- tasksmyform.zzz.com.ua -->
-		<!-- login + parol: u51408_ -->
-		<!-- ip-address 109.95.211.97 -->
-		<!-- server ip-address: localhost, 109.95.211.97 -->
